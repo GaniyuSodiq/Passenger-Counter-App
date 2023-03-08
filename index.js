8b12f881282ac6860c0f72f89c8b04f3c2872962
Sodiq Ganiyu <gsolaniyi@gmail.com>
2023-03-08 08:28:45 +0100

    function to increament counts

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
-// document.getElementById("count").innerText = 5
+let lapsCompleted = 0
 
-
-// function increament() {
-//     console.log("The button was clicked!")
-// }
-
-let lap1 = 34
-let lap2 = 33
-let lap3 = 36
-
-function sumLapTimes() {
-    let totalTime = lap1 + lap2 + lap3
-    console.log(totalTime)
+function increamentLaps() {
+    lapsCompleted = lapsCompleted + 1
 }
 
-sumLapTimes()
+increamentLaps()
+increamentLaps()
+increamentLaps()
+
+console.log(lapsCompleted)
