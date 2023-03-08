07dd5614ce281fbeda7e87088111031578784ee4
Sodiq Ganiyu <gsolaniyi@gmail.com>
2023-03-08 08:01:57 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// document.getElementById("count").innerText = 5
+
+
+// function increament() {
+//     console.log("The button was clicked!")
+// }
+
+function call42() {
+    console.log(42)
+}
+
+call42()
