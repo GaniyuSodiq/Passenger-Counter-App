e0714b5de4d973a4fda0841b2a0797c77261d32d
Sodiq Ganiyu <gsolaniyi@gmail.com>
2023-03-08 10:35:49 +0100

    working with Strings and numbers in JavaScript

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
-let count = 0
+// let count = 0
 
-let countEl = document.getElementById("count-el")
+// let countEl = document.getElementById("count-el")
 
 
-function increament() {
-    count = count + 1
-    countEl.innerText = count
-}
+// function increament() {
+//     count = count + 1
+//     countEl.innerText = count
+// }
 
-function save() {
-    console.log(count)
-}
+// function save() {
+//     console.log(count)
+// }
+
+let username = "Sodiq"
+
+let message = "You have three new notfications"
+
+let messageToUser = message  + ", " + username + "!"
+
+console.log(messageToUser)
+
+let name = "Sidiq"
+let greeting = "Hi, " + name + " " +"Welcome back"
+
+
+
+let welcomeEL = document.getElementById("welcome-el")
+
+welcomeEL.innerText = greeting
